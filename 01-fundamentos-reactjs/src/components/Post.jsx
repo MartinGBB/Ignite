b039b004179ae2af12e2af8720a73992af2223fb
Martin <escorpmartin97@gmail.com>
2022-07-11 16:50:07 -0300

    feat: coloca texto do comentario

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -16,8 +16,21 @@ export function Post() {
 
         <time
           title="11 de fevereiro ás 04:12"
-          dateTime="2022-02-2022 04:12:22">Publicado há uma hora</time>
+          dateTime="2022-02-2022 04:12:22"
+        >
+          Publicado há uma hora
+        </time>
       </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+        <p>👉 <a href="">jane.design/doctorcare</a></p>
+
+        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+      </div>
     </article>
   )
 }
