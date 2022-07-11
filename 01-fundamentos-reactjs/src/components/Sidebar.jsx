e0944eca89212b890715134277137031d4265e40
Martin <escorpmartin97@gmail.com>
2022-07-11 10:23:56 -0300

    feat: adiciona avatar de perfil

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -7,7 +7,11 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=500"
         className={styles.cover} 
       />
-      <div className={ styles.profile}>
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/81588649?v=4"
+        />
         <strong>Martin Brazón</strong>
         <span>Web Developer</span>
         <footer>
