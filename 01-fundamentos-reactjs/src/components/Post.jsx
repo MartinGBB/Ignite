17d1d57e0e90666ea21eb662f1c126baa4c476b9
Martin <escorpmartin97@gmail.com>
2022-07-11 16:43:53 -0300

    feat: coloca dados do componente Post e aprandi a utilizar a tag time

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,5 +1,23 @@
+import styles from './Post.module.css';
+
 export function Post() {
   return (
-    <h1>Hello world</h1>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://avatars.githubusercontent.com/u/81588649?v=4" />
+            <div className={styles.authorInfo}>
+              <strong>Martin Brazon</strong>
+              <span>Web Developer</span>
+            </div>
+        </div>
+
+        <time
+          title="11 de fevereiro ás 04:12"
+          dateTime="2022-02-2022 04:12:22">Publicado há uma hora</time>
+      </header>
+    </article>
   )
 }
