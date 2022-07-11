a0d4960cf0f1f773501feca6dbda67cec8c337ba
Martin <escorpmartin97@gmail.com>
2022-07-11 10:21:06 -0300

    style: configura como irao se mostrar os items no sidebar

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -3,7 +3,10 @@ import styles from './Sidebar.module.css';
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=500" />
+      <img
+        src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=500"
+        className={styles.cover} 
+      />
       <div className={ styles.profile}>
         <strong>Martin Brazón</strong>
         <span>Web Developer</span>
