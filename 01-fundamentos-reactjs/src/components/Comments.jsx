805381bd1898a464fb46a0f6ba1b438ce1fa5ff3
Martin <escorpmartin97@gmail.com>
2022-07-12 17:42:49 -0300

    feat: adiciona iconos e dados do comentario

--- 01-fundamentos-reactjs/src/components/Comments.jsx
+++ 01-fundamentos-reactjs/src/components/Comments.jsx
@@ -1,7 +1,38 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 
 export function Comment() {
   return (
-    <div className={styles}></div>
+    <div className={styles.comment}>
+      <img src="https://github.com/MartinGBB.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Martin Brazon</strong>
+              <time
+                title="11 de fevereiro ás 04:12"
+                dateTime="2022-02-2022 04:12:22"
+              >
+                Cerca de 1h
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={20}/>
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
   )
 }
