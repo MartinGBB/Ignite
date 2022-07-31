185a3fdbf16b9f7068ad982be6b571f8e0b28469
Martin <escorpmartin97@gmail.com>
2022-07-31 17:59:07 -0300

    feat: configura responsividade

--- 01-fundamentos-reactjs-ts/src/components/Login.tsx
+++ 01-fundamentos-reactjs-ts/src/components/Login.tsx
@@ -1,7 +1,6 @@
 import styles from './Login.module.css';
 import photo from '../assets/felix-rostig-UmV2wr-Vbq8-unsplash 1.png'
 import igniteLogo from '../assets/ignite-logo.svg';
-import { Header } from './Header';
 
 export function Login() {
   return (
@@ -9,8 +8,9 @@ export function Login() {
       <img src={photo} />
 
       <div className={styles.form}>
-        <Header />
         <div className={styles.contentInfo}>
+          <img src={ igniteLogo } alt="logo do ignite" />
+          <span>IgniteFeed</span>
           <h1>Escolha uma opção para entrar</h1>
           <p>Utilize algum dos serviços abaixo para entrar na nossa plataforma.</p>
         </div>
