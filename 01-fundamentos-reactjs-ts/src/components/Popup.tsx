80f1f7299e5d5e495a4621a88ebd5ba76ce1c214
Martin <escorpmartin97@gmail.com>
2022-08-01 21:29:23 -0300

    feat: adiciona itens que tera o componente

--- 01-fundamentos-reactjs-ts/src/components/Popup.tsx
+++ 01-fundamentos-reactjs-ts/src/components/Popup.tsx
@@ -2,8 +2,16 @@ import styles from './Popup.module.css';
 
 export function Popup() {
   return (
-    <div className={styles.popupContent}>
-      
+    <div className={styles.content}>
+      <div className={styles.popUp}>
+        <h1>Excluir comentário</h1>
+        <p>Você tem certeza que gostaria de excluir este comentário?</p>
+
+        <div>
+          <button>Cancelar</button>
+          <button>Sim, excluir</button>
+        </div>
+      </div>
     </div>
   );
 }
