d2a13d548b24d8800728dd675a6311c88eadae76
Martin <escorpmartin97@gmail.com>
2022-08-11 19:58:20 -0300

    feat: adiciona paleta de cores

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,15 +1,12 @@
 import { useState } from 'react';
 import { ThemeProvider } from 'styled-components'
 import { Button } from "./components/Buttons";
-import { defaultTheme, ThemeBlue } from './styles/theme/default';
-import { GlobalStyle } from './styles/theme/global';
+import { defaultTheme } from './styles/theme/default';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   const [theme, setTheme] = useState(defaultTheme)
 
-  function handleTheme() {
-    theme === defaultTheme ? setTheme(ThemeBlue) : setTheme(defaultTheme)
-  }
   return (
     <ThemeProvider theme={theme}>
       <Button />
@@ -17,7 +14,6 @@ export function App() {
       <Button />
       <Button />
 
-      <button onClick={handleTheme}>mudar tema</button>
       <GlobalStyle />
     </ThemeProvider>
   )
