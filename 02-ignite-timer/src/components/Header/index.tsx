03430c723bedde3657d03e7551dd61424719a7ce
Martin <escorpmartin97@gmail.com>
2022-08-11 22:18:00 -0300

    feat: instala biblioteca de iconos e adiciona iconos e logo ignite

--- 02-ignite-timer/src/components/Header/index.tsx
+++ 02-ignite-timer/src/components/Header/index.tsx
@@ -1,9 +1,20 @@
 import { HeaderContainer } from './styles'
+import { Timer, Scroll } from 'phosphor-react'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <h1>Header</h1>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <a href="">
+          <Timer size={24} />
+        </a>
+        <a href="">
+          <Scroll size={24} />
+        </a>
+      </nav>
     </HeaderContainer>
   )
 }
