90e1171b96248e4f992b18afdfe846bea0bcea5a
Martin <escorpmartin97@gmail.com>
2022-08-12 10:59:01 -0300

    style: customiza botao e adiciona configuracoes de desabilidar

--- 02-ignite-timer/src/pages/Home/styles.ts
+++ 02-ignite-timer/src/pages/Home/styles.ts
@@ -49,3 +49,26 @@ export const Separator = styled.div`
   display: flex;
   justify-content: center;
 `
+export const StartCountButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  font-weight: bold;
+  cursor: pointer;
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-700']};
+  }
+`
