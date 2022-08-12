2dc1007d2a4de3debe4b55c20492e2330a826aeb
Martin <escorpmartin97@gmail.com>
2022-08-12 11:38:52 -0300

    style: corrige aliniacao do formulario

--- 02-ignite-timer/src/pages/Home/styles.ts
+++ 02-ignite-timer/src/pages/Home/styles.ts
@@ -18,6 +18,7 @@ export const HomeContainer = styled.main`
 export const FormContainer = styled.div`
   width: 100%;
   display: flex;
+  align-items: center;
   justify-content: center;
   gap: 0.5rem;
   color: ${(props) => props.theme['gray-100']};
