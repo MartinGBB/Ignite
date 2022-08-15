f59d5c18e13b3f59c2308f5c4a7e6ac0151930d3
Martin <escorpmartin97@gmail.com>
2022-08-15 17:34:58 -0300

    adiciona data de quando foi feita a tarefa e adiciona idioma portuges

--- 02-ignite-timer/src/pages/History/index.js.tsx
+++ 02-ignite-timer/src/pages/History/index.js.tsx
@@ -1,4 +1,6 @@
 import { useContext } from 'react'
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { CyclesContext } from '../../context/CyclesContext'
 import { HistoryContainer, HistoryList, StatusContent } from './styles'
 
@@ -22,9 +24,14 @@ export function History() {
             {cycles.map((cycle) => {
               return (
                 <tr key={cycle.id}>
-                  <td>cycle.task</td>
+                  <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
-                  <td>cycle.startDate.toISOString()</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
                   <td>
                     {cycle.finishedDate && (
                       <StatusContent statusColor="green">
