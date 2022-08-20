776d625a4ac4561e6258eb6193bb227151b7990b
Martin <escorpmartin97@gmail.com>
2022-08-20 13:47:38 -0300

    refator: melhora as actions

--- 02-ignite-timer/src/context/CyclesContext.tsx
+++ 02-ignite-timer/src/context/CyclesContext.tsx
@@ -1,5 +1,9 @@
 import React, { createContext, ReactNode, useReducer, useState } from 'react'
-import { ActionTypes } from '../reducers/cycles/actions'
+import {
+  addCycleAction,
+  interruptCurrentCycleAction,
+  markCurrentCycleAsFinishedAction,
+} from '../reducers/cycles/actions'
 import { Cycle, cyclesReducer } from '../reducers/cycles/reducers'
 
 interface CreateCycleData {
@@ -43,12 +47,7 @@ export function CyclesContextProvider({
   }
 
   function markCurrentCycleAsFinished() {
-    dispatch({
-      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-      payload: {
-        activeCycleId,
-      },
-    })
+    dispatch(markCurrentCycleAsFinishedAction())
   }
 
   function createNewCycle(data: CreateCycleData) {
@@ -59,23 +58,13 @@ export function CyclesContextProvider({
       startDate: new Date(),
     }
 
-    dispatch({
-      type: ActionTypes.ADD_NEW_CYCLE,
-      payload: {
-        newCycle,
-      },
-    })
+    dispatch(addCycleAction(newCycle))
 
     setAmountSecoundsPassed(0)
   }
 
   function interruptCurrentCycle() {
-    dispatch({
-      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-      payload: {
-        activeCycleId,
-      },
-    })
+    dispatch(interruptCurrentCycleAction())
   }
 
   return (
