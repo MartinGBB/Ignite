998ba4311b8716bfb67b9971b86e788104c7a3e0
Martin <escorpmartin97@gmail.com>
2022-10-12 17:26:48 -0300

    fix: erro de escrita na data

--- 03-dt-money/src/pages/Transations/index.tsx
+++ 03-dt-money/src/pages/Transations/index.tsx
@@ -21,13 +21,13 @@ export function Trasactions() {
                 return (
                   <tr key={transaction.id}>
                     <td width="50%">{transaction.description}</td>
-                      <td>
-                        <PriceHighLight variant={transaction.type}>
-                          {transaction.price}
-                        </PriceHighLight>
-                      </td>
-                      <td>{transaction.category}</td>
-                    <td>{transaction.createAt}</td>
+                    <td>
+                      <PriceHighLight variant={transaction.type}>
+                        {transaction.price}
+                      </PriceHighLight>
+                    </td>
+                    <td>{transaction.category}</td>
+                    <td>{transaction.createdAt}</td>
                   </tr>
                 )
               })}
